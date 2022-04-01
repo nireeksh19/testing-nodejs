571e01c99019831e031e06261a55f12a5fa11883
nireeksh-shetty <niree999@gmail.com>
2022-04-01 21:05:41 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require("fs");
+
+const home = fs.readFileSync("./index.html");
+
+const PORT = 2000;
+const hostname = "localhost";
+
+const server = http.createServer((req,res)=>{
+    if (req.url==="/") {
+        return res.end(home);
+    }
+    if (req.url==="/about") {
+        return res.end("<h1>ABOUT PAGE</h1>");
+    }
+    if (req.url==="/contact") {
+        return res.end("<h1>CONTACT PAGE</h1>");
+    }
+    if (req.url==="/service") {
+        return res.end("<h1>SERVICE PAGE</h1>");
+    }
+    else{
+        return res.end("<h1>404 ERROR PAGE</h1>")
+    }
+});
+server.listen(PORT,hostname,()=>{
+    console.log(`Server is working on http://${hostname}:${PORT}`)
+});
